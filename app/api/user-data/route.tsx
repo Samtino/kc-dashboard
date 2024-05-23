9c6a21de7a9c2aa9239d5436d3510d20637ecbf6
Samtino <35015007+Samtino@users.noreply.github.com>
2024-05-22 21:08:46 -0700

    Add error handling

--- app/api/user-data/route.tsx
+++ app/api/user-data/route.tsx
@@ -5,7 +5,17 @@ import { User } from '@/app/lib/types';
 
 export async function GET() {
   try {
+    const key = process.env.SECRET;
+
+    if (!key) {
+      return NextResponse.json({ error: 'No key provided' }, { status: 400 });
+    }
+
     const encryptedData = cookies().get('user')?.value as string;
+    if (!encryptedData) {
+      return NextResponse.json({ error: 'No user cookie found' }, { status: 404 });
+    }
+
     const token = encryptedData.substring(1, encryptedData.length - 1);
     const user: User = await decrypt(token);
 
