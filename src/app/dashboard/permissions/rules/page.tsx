31890af498f35e6a5a1a219465486370eadfd538
Samtino <35015007+Samtino@users.noreply.github.com>
2024-05-23 19:01:50 -0700

    Add redirects to SOPs and Rules to WIP pages

--- src/app/dashboard/permissions/rules/page.tsx
+++ src/app/dashboard/permissions/rules/page.tsx
@@ -1,8 +1,21 @@
+import { Button } from '@mantine/core';
+import Link from 'next/link';
+
 export default function SopsPage() {
   return (
     <>
       <h1>Liberation Rules</h1>
       <p>Coming soon!</p>
+
+      <br />
+
+      <Button
+        component={Link}
+        href="https://forums.karmakut.com/index.php?/forumslibinforules/armalibsops/"
+        target="_blank"
+      >
+        View Rules on the KC Forums
+      </Button>
     </>
   );
 }
--- src/app/dashboard/permissions/sops/page.tsx
+++ src/app/dashboard/permissions/sops/page.tsx
@@ -1,8 +1,23 @@
+'use client';
+
+import { Button } from '@mantine/core';
+import Link from 'next/link';
+
 export default function SopsPage() {
   return (
     <>
       <h1>Liberation SOPs</h1>
       <p>Coming soon!</p>
+
+      <br />
+
+      <Button
+        component={Link}
+        href="https://forums.karmakut.com/index.php?/forumslibinforules/armalibsops/"
+        target="_blank"
+      >
+        View SOPs on the KC Forums
+      </Button>
     </>
   );
 }
