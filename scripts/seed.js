2104d544223fa7e89dd8fb8b7384dcbcee0d193c
Samtino <35015007+Samtino@users.noreply.github.com>
2024-06-24 23:11:39 -0700

    Updated seed script to add questions to the exams (only CC so far)

--- scripts/seed.js
+++ scripts/seed.js
@@ -5,6 +5,63 @@ const permissions = [
   {
     name: 'Company Command',
     required_hours: 100,
+    questions: [
+      {
+        text: 'How many strikes will cause you to lose permissions for Company Commander?',
+        options: ['4', '2', '3'],
+        correct_answer: '2',
+      },
+      {
+        text: 'True or False: Company Commander is permitted to operate outside of a FOB or Operations Base?',
+        options: ['True', 'False'],
+        correct_answer: 'True',
+      },
+      {
+        text: 'True or False: Company Commander should have complete knowledge of ALL Standard Operating Procedures?',
+        options: ['True', 'False'],
+        correct_answer: 'True',
+      },
+      {
+        text: 'Which Frequencies are you required to monitor as Company Command?',
+        options: ['LR 031.000 & SR 160.000', 'LR 080.000 & SR 070.000', 'LR 030.000 & SR 036.000'],
+        correct_answer: 'LR 030.000 & SR 036.000',
+      },
+      {
+        text: 'What takes priority for building assets?',
+        options: ['Armor', 'Transport', 'Air CAS'],
+        correct_answer: 'Transport',
+      },
+      {
+        text: 'Who has authority over Armored assets?',
+        options: ['Platoon Leaders', 'Company Command', 'Squad Leaders'],
+        correct_answer: 'Platoon Leaders',
+      },
+      {
+        text: 'What frequency is used to contact Platoon JFOs for requests?',
+        options: ['LR 031.000', 'LR 080.000', 'LR 040.000'],
+        correct_answer: 'LR 080.000',
+      },
+      {
+        text: 'True or False: Assassin 6-6 can give direct orders to Assassin squads.',
+        options: ['True', 'False'],
+        correct_answer: 'False',
+      },
+      {
+        text: 'How many Shade crew members are required to operate mortars outside of a FOB?',
+        options: ['1', '2', '3'],
+        correct_answer: '2',
+      },
+      {
+        text: 'Are you permitted to fly the MQ-9 Reaper drone?',
+        options: ['Yes', 'No'],
+        correct_answer: 'No',
+      },
+      {
+        text: 'Can you authorize danger close fire missions?',
+        options: ['Yes', 'No'],
+        correct_answer: 'No',
+      },
+    ],
   },
   {
     name: 'Platoon Leader',
@@ -60,15 +117,39 @@ const permissions = [
 
 async function main() {
   console.log('Deleting all permissions...');
-  await prisma.permission.deleteMany();
-  console.log('Permissions deleted');
+  try {
+    await prisma.permission.deleteMany();
+    console.log('Permissions deleted');
+  } catch (error) {
+    console.error(error);
+  }
 
   console.log('Seeding permissions...');
   for (const permission of permissions) {
-    await prisma.permission.create({
-      data: permission,
-    });
-    console.log(`Permission ${permission.name} seeded`);
+    const permData = {
+      name: permission.name,
+      required_hours: permission.required_hours,
+      prerequisites: permission.prerequisites || [],
+      asset_exam: permission.asset_exam || false,
+      questions: permission.questions
+        ? {
+            create: permission.questions.map((question) => ({
+              text: question.text,
+              options: question.options,
+              correct_answer: question.correct_answer,
+            })),
+          }
+        : undefined,
+    };
+
+    try {
+      await prisma.permission.create({
+        data: permData,
+      });
+      console.log(`Permission ${permission.name} seeded`);
+    } catch (error) {
+      console.error(`Error seeding permission ${permission.name}:`, error);
+    }
   }
 }
 
