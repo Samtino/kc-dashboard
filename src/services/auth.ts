97b246b62ce0a38aa153741e70bb92f98214387b
Samtino <35015007+Samtino@users.noreply.github.com>
2024-06-28 16:45:46 -0700

    Fix logout button

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -18,6 +18,8 @@ export async function login(): Promise<void> {
 }
 
 export async function logout(): Promise<void> {
+  // 'use client';
+
   const userCookie = cookies().get('user');
 
   if (!userCookie) {
