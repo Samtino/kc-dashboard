15ec36676690fda110b42e2e8fbe13623d36e6ac
Samtino <35015007+Samtino@users.noreply.github.com>
2024-06-29 20:02:04 -0700

    Fixed infinite loop in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,51 +1,65 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { User } from '@prisma/client';
 import { decrypt } from './services/encryption';
+import { UserData } from '@/lib/types';
 
 export async function middleware(request: NextRequest) {
   const cookie = request.cookies.get('user');
-  const { user }: { user: User | undefined } = cookie ? await decrypt(cookie.value) : undefined;
+  const userData: UserData | undefined = cookie ? await decrypt(cookie.value) : undefined;
+  const user = userData?.user;
   const path = request.nextUrl.pathname;
 
+  // Redirect to /dashboard if the user is authenticated and is on the root path
   if (path === '/') {
-    if (!user) {
-      // TODO: Redirect to /login instead after implementing login page
-      return NextResponse.redirect(new URL('/', request.url));
+    if (user) {
+      return NextResponse.redirect(new URL('/dashboard', request.url));
     }
-    return NextResponse.redirect(new URL('/dashboard', request.url));
+
+    // Allow access to the root path if the user is not authenticated
+
+    return NextResponse.next();
   }
 
-  if (!user) {
+  // Redirect to the root path if the user is not authenticated and is trying to access a protected route
+  if (!user && path !== '/') {
     return NextResponse.redirect(new URL('/', request.url));
   }
 
-  if (user.roles.includes('SYS_ADMIN')) {
+  // Sys-admins can bypass all checks
+  if (user && user.roles.includes('SYS_ADMIN')) {
     return NextResponse.next();
   }
 
-  if (path.startsWith('/dashboard')) {
+  // Allow access to /dashboard for authenticated users
+  if (user && path.startsWith('/dashboard')) {
     return NextResponse.next();
   }
 
-  if (user.roles.includes('KOG') && path.startsWith('/dashboard/kog')) {
+  // Allow access to specific roles for specific paths
+  if (user && user.roles.includes('KOG') && path.startsWith('/dashboard/kog')) {
     return NextResponse.next();
   }
 
-  if (user.roles.includes('KT') && path.startsWith('/dashboard/kt')) {
+  if (user && user.roles.includes('KT') && path.startsWith('/dashboard/kt')) {
     return NextResponse.next();
   }
 
   if (
-    user.roles.includes('ADMIN') ||
-    (user.roles.includes('COMMUNITY_STAFF') && path.startsWith('/dashboard/admin'))
+    user &&
+    (user.roles.includes('ADMIN') || user.roles.includes('COMMUNITY_STAFF')) &&
+    path.startsWith('/dashboard/admin')
   ) {
     return NextResponse.next();
   }
 
-  if (user.roles.includes('COMMUNITY_STAFF') && path.startsWith('/dashboard/admin/manage')) {
+  if (
+    user &&
+    user.roles.includes('COMMUNITY_STAFF') &&
+    path.startsWith('/dashboard/admin/manage')
+  ) {
     return NextResponse.next();
   }
 
+  // If none of the above conditions are met, allow the request to proceed
   return NextResponse.next();
 }
 
